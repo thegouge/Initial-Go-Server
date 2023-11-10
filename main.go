00d40a88a9603e4cfc481c6879f63a11134308de
thegouge <thegouge42@gmail.com>
2023-11-10 13:25:27 -0800

    Setting up the initial server

--- main.go
+++ main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	const PORT string = "8000"
+	mux := http.NewServeMux()
+
+	corsMux := middlewareCors(mux)
+
+	server := http.Server{
+		Addr:    "localhost:" + PORT,
+		Handler: corsMux,
+	}
+
+	fmt.Printf("Booting up Server on port %v\n", PORT)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
